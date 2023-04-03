257a40d1daf7b353161294a463f6b8d65d5d43c9
Filipe Moreira <filipe.moreira@nationbuilder.com>
2023-04-03 16:50:41 +0100

    fix: `ActiveRecord::Base.clear_active_connections!` without threads

--- app/controllers/event_ingestor_controller.rb
+++ app/controllers/event_ingestor_controller.rb
@@ -1,13 +1,11 @@
 class EventIngestorController < ApplicationController
   def new
-    Thread.new do
-      ClickEvent.create!(signup_id: rand(100_000),
-                         mailing_id: rand(10_000),
-                         broadcaster_id: rand(1_000))
-    rescue StandardError => e
-      Rails.log.error("SDMKF: Error raised => #{e}")
-    ensure
-      ActiveRecord::Base.clear_active_connections!
-    end
+    ClickEvent.create!(signup_id: rand(100_000),
+                       mailing_id: rand(10_000),
+                       broadcaster_id: rand(1_000))
+  rescue StandardError => e
+    Rails.log.error("SDMKF: Error raised => #{e}")
+  ensure
+    ActiveRecord::Base.clear_active_connections!
   end
 end
