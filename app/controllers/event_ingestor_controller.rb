4cdafffc66656ff22184ff17a6a8c1d786cf287a
Filipe Moreira <filipe.moreira@nationbuilder.com>
2023-04-04 15:08:04 +0100

    fix: using sample of four mailing IDs

--- app/controllers/event_ingestor_controller.rb
+++ app/controllers/event_ingestor_controller.rb
@@ -1,9 +1,13 @@
 class EventIngestorController < ApplicationController
+  MAILING_ID = [200, 300, 400, 500].freeze
+
   def new
     ClickEvent.create!(signup_id: rand(100_000),
-                       mailing_id: rand(10_000),
+                       mailing_id: MAILING_ID.sample,
                        broadcaster_id: rand(1_000))
   rescue StandardError => e
-    Rails.log.error("SDMKF: Error raised => #{e}")
+    Rails.logger.error("SDMKF: Error raised => #{e}")
+  ensure
+    ActiveRecord::Base.clear_active_connections!
   end
 end
