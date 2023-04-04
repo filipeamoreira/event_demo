e7649b2238e62e36c4681c120c27c7d0e4b5e0e7
Filipe Moreira <filipe.moreira@nationbuilder.com>
2023-04-04 15:07:52 +0100

    add: various scopes

--- app/models/click_event.rb
+++ app/models/click_event.rb
@@ -1,2 +1,19 @@
 class ClickEvent < ApplicationRecord
+  # Scopes
+
+  scope :last_month, -> { where('created_at > ?', 1.month.ago) }
+  scope :last_week, -> { where('created_at > ?', 1.week.ago) }
+  scope :last_hour, -> { where('created_at > ?', 1.hour.ago) }
+  scope :yesterday, -> { where('DATE(created_at) = ?', 1.day.ago.to_date) }
+  scope :today, -> { where('DATE(created_at) = ?', Date.today) }
+  scope :total, -> { all.size }
+
+  scope :counts_per, ->(time_dimension) {
+    select("time_bucket('#{time_dimension}', created_at) as time, count(1) as total")
+      .group(:time)
+      .order(:time)
+      .map { |result| [result.time, result.total] }
+  }
+
+  scope :for_mailing, ->(mailing_id) { where(mailing_id: mailing_id) }
 end
